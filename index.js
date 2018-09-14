3051a31b8dd8109fa5b630095fbccdebf3e1f132
fantastic-refactoring-2336 <naomijoyceb@gmail.com>
2018-09-14 17:32:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
 function dwarfRollCall(dwarves) {
+  
+  var list = ""; //creates an empty string array 
+  
+  for (var i = 0; i < dwarves.length; i++){
+     list+= (i+1) + ". " + dwarves[i] + " ";
+  }   //increments list by the expression
+  return list;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  //changes each element to uppercase and adds '!'
+  
+  for (var i = 0; i < planeteerCalls.length; i++){
+    planeteerCalls[i] += planeteerCalls[i].toUpperCase() + "!";
+  }
+  return planeteerCalls;
 }
 
 function longPlaneteerCalls(words) {
+  //loops through each element and checks if char = 4+
+  
+  var x;
+  
+  for (var i = 0; i < words.length; i++){
+    if (words.length > 4){
+      x = true;
+    } else {
+      x = false;
+    }
+  }
+  return x;
 }
 
 function findTheCheese (foods) {
+  
+  var cheese = ["cheddar", "gouda", "camembert"];
+  var arr = [];
+  
+  for (var i = 0; i<cheese.length; i++){
+    if(foods.indexOf(cheese[i]) >= 0){
+      return arr += cheese[i];
+    } else {
+      return "no cheese!";
+    }
+  }
+  
 }
